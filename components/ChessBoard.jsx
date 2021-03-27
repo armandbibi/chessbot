67b7289b2f1fcaf3307303800787187fad931382
Vincent Bauguen <vincent.bauguen@gmail.com>
2021-03-27 17:34:00 +0100

    add ts + eslint and prettier in dependencies. Add configs files for prettier/eslint and typescript

--- components/ChessBoard.jsx
+++ components/ChessBoard.jsx
@@ -1,77 +1,68 @@
 import React, { Component, useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Alert, Dimensions } from 'react-native';
 import { Table, TableWrapper, Row, Cell, Rows } from 'react-native-table-component';
-import { fenToPieces } from "./Piece"
+import { fenToPieces } from './Piece';
 
-import AbsolutePiecePosition from "./PiecePositions"
+import AbsolutePiecePosition from './PiecePositions';
 
 import { connect } from 'react-redux';
 import { changeBoard } from '../store/actions/board';
 import { bindActionCreators } from 'redux';
-import { ChessBoardSquare } from './ChessboardSquare.jsx';
+import { ChessBoardSquare } from './ChessboardSquare';
 
-
-const startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+const startingPosition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
 
 export class ChessBoard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableData : createBoard()
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableData: createBoard(),
+        };
     }
 
-  }
+    render() {
+        const state = this.state;
 
-  render() {
-    
-    const state = this.state;
-    
-    return (
-      <View style={styles.container}>
-        <Table borderStyle={{ borderWidth: 0, borderColor: '#c8e1ff' }}>
-          <Rows data={state.tableData} style={styles.board} />
-        </Table>
-        <AbsolutePiecePosition></AbsolutePiecePosition>
-      </View>
-    )
-  }
+        return (
+            <View style={styles.container}>
+                <Table borderStyle={{ borderWidth: 0, borderColor: '#c8e1ff' }}>
+                    <Rows data={state.tableData} style={styles.board} />
+                </Table>
+                <AbsolutePiecePosition></AbsolutePiecePosition>
+            </View>
+        );
+    }
 }
 
 const screenWidth = Dimensions.get('screen').width;
 
 const styles = StyleSheet.create({
-  container: { marginTop: 150, backgroundColor: '#fff' },
-  text: { margin: 6, backgroundColor: 'red' },
-  board: { width: screenWidth, height: screenWidth / 8 },
-
+    container: { marginTop: 150, backgroundColor: '#fff' },
+    text: { margin: 6, backgroundColor: 'red' },
+    board: { width: screenWidth, height: screenWidth / 8 },
 });
 
-
 function createBoard() {
+    const tab = [8];
 
-  const tab = [8];
-  
     for (let file = 0; file < 8; file++) {
-      tab[file] = [8];
-      for (let rank = 0; rank < 8; rank++) {
-        let color = ((file + rank) % 2 != 0) ? 'black' : 'white';
-        tab[file][rank] = <ChessBoardSquare  color={color}/>
-      }
+        tab[file] = [8];
+        for (let rank = 0; rank < 8; rank++) {
+            let color = (file + rank) % 2 != 0 ? 'black' : 'white';
+            tab[file][rank] = <ChessBoardSquare color={color} />;
+        }
     }
-  
+
     return tab;
 }
 
-const mapStateToProps = state => ({
-  board: state.board.board,
+const mapStateToProps = (state) => ({
+    board: state.board.board,
 });
 
-const ActionCreators = Object.assign(
-  {},
-  changeBoard,
-);
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(ActionCreators, dispatch),
+const ActionCreators = Object.assign({}, changeBoard);
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(ActionCreators, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChessBoard)
+export default connect(mapStateToProps, mapDispatchToProps)(ChessBoard);
